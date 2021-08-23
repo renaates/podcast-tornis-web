7b104a896a21d7c596c35167c42d6512302e5389
Renate Spirga <spirgarenate@gmail.com>
2021-08-23 20:18:23 +0300

    add "active" attribute to person element

--- src/person/Person.js
+++ src/person/Person.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Person.css";
 
-export const Person = ({ image, name, role }) => {
+export const Person = ({ image, name, role, active=true}) => {
   return (
     <div className="person-item">
-      <img className="person-image" src={image} alt={name} />
-      <div className="person-description">
+      <img className="person-image" src={image} alt={name} data-status={active}/>
+      <div className="person-description" data-status={active}>
         <h4>{name}</h4>
         <p>{role}</p>
       </div>
