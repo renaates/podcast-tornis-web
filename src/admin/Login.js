9fd6779a82471786833cac1b69d4ec7e6e19e398
Renate Spirga <spirgarenate@gmail.com>
2025-01-02 16:20:46 +0200

    add rerouting from /login if authenticated

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import firebase from "firebase/app";
 import "firebase/auth";
